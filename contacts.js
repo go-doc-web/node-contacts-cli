8dcae57ed3c6943bd248562205198f3c5cf49ede
Go.Doc <97858843+OlegDoc@users.noreply.github.com>
2023-03-26 15:34:50 +0300

    Делает функцию добовления контакта

--- contacts.js
+++ contacts.js
@@ -1,18 +1,36 @@
 const fs = require('fs/promises');
+
 const path = require('path');
 
 const contactsPath = path.join(`${__dirname}`, './db/contacts.json');
+const { nanoid } = require('nanoid');
 
 const listContacts = async () => {
-    const contacts = await fs.readFile(contactsPath);
-    return JSON.parse(contacts)
-}
+   const contacts = await fs.readFile(contactsPath);
+   return JSON.parse(contacts);
+};
 
-const getContactById = async (contactId) => {
-    const contacts = await listContacts();
-    const result = contacts.find(item => item.id === contactId);
-    return result || null;
-}
+const getContactById = async contactId => {
+   const contacts = await listContacts();
+   const result = contacts.find(item => item.id === contactId);
+   return result || null;
+};
 
+const addContact = async ({ name, email, phone }) => {
+   const contacts = await listContacts();
+   const newContact = {
+      id: nanoid(),
+      name,
+      email,
+      phone,
+   };
+   contacts.push(newContact);
+   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+   return newContact;
+};
 
-module.exports = {listContacts,getContactById};
+module.exports = {
+   listContacts,
+   getContactById,
+   addContact,
+};
--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const contacts = require('./contacts');
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
-    switch (action) {
-        case 'list':
-            const contactsAll = await contacts.listContacts();
-            return console.log(contactsAll);
-        case "get":
-            const getById = await contacts.getContactById(id);
-            return console.log(getById);
-    }
-}
+   switch (action) {
+      case 'list':
+         const contactsAll = await contacts.listContacts();
+         return console.log(contactsAll);
+      case 'get':
+         const getById = await contacts.getContactById(id);
+         return console.log(getById);
+      case 'add':
+         const add = await contacts.addContact({
+            name,
+            email,
+            phone,
+         });
+         return console.log(add);
+   }
+};
 
 // invokeAction({ action: 'list' });
-invokeAction({ action: 'get' ,id:'AeHIrLTr6JkxGE6SN-0Rw'});
+// invokeAction({ action: 'get' ,id:'AeHIrLTr6JkxGE6SN-0Rw'});
+invokeAction({
+   action: 'add',
+   name: 'Oleg',
+   email: 'oleg@compileFunction.ua',
+   phone: '398-235-89',
+});
