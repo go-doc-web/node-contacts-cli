59fd96249909ad15bda0c78671f4e3da32b913d3
Go.Doc <97858843+OlegDoc@users.noreply.github.com>
2023-03-26 19:29:31 +0300

    Commandergit add .git add .

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const contacts = require('./contacts');
 
+const { Command } = require('commander');
+const program = new Command();
+
+program
+   .option('-a, --action <type>', 'choose action')
+   .option('-i, --id <type>', 'user id')
+   .option('-n, --name <type>', 'user name')
+   .option('-e, --email <type>', 'user email')
+   .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
 const invokeAction = async ({ action, id, name, email, phone }) => {
    switch (action) {
       case 'list':
          const contactsAll = await contacts.listContacts();
-         return console.log(contactsAll);
+         return console.table(contactsAll);
       case 'get':
          const getById = await contacts.getContactById(id);
          return console.log(getById);
@@ -24,6 +38,8 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
    }
 };
 
+invokeAction(argv);
+
 // invokeAction({ action: 'list' });
 // invokeAction({ action: 'get' ,id:'AeHIrLTr6JkxGE6SN-0Rw'});
 // invokeAction({
@@ -33,4 +49,4 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
 //    phone: '398-235-89',
 // });
 
-invokeAction({ action: 'remove', id: 'drsAJ4SHPYqZeG-83QTVW' });
+// invokeAction({ action: 'remove', id: 'drsAJ4SHPYqZeG-83QTVW' });
